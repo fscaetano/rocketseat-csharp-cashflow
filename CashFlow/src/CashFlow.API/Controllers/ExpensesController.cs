52e4469883dc9954550b4e2d39032caa77eaa73f
Fabricio da Silva Caetano <fscaetano@gmail.com>
2024-12-27 11:56:02 -0300

    Added expenses endpoint

---
+++ CashFlow/src/CashFlow.API/Controllers/ExpensesController.cs
@@ -0,0 +1,18 @@
+﻿using CashFlow.Communication.Requests;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CashFlow.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ExpensesController : ControllerBase
+    {
+
+        [HttpPost]
+        public IActionResult Register([FromBody] RequestExpenseJson request)
+        {
+            return Created();
+        }
+    }
+}
